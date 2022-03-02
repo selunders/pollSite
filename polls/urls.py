ba610f74fa69fcb6c53c3b8d6391a00dac2a5995
selunders <sethlunders@gmail.com>
2022-03-02 15:34:04 -0800

    Switch to djangos's generic views

--- polls/urls.py
+++ polls/urls.py
@@ -6,13 +6,13 @@ from . import views
 app_name = 'polls'
 urlpatterns = [ 
     # /polls/
-    path('', views.index, name='index'),
+    path('', views.IndexView.as_view(), name='index'),
 
     # /polls/5/
     # the 'name' value as called by the {% url %} template tag
-    path('<int:question_id>/', views.detail, name='detail'),
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
     # /polls/5/results/
-    path('<int:question_id>/results/', views.results, name='results'),
+    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
     # /polls/5/votes/
     path('<int:question_id>/vote/', views.vote, name='vote')
     
